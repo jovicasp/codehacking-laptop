39d6f01a0d8db81de79ee87786186f41e49d15ee
jovica-laravel <jovica@laravelcourse.com>
2022-03-25 00:30:13 +0100

    Commit4: create user-index page with table and user-create page with forms for all properties, plugin laravelcollective-html, setting AdminUsersController methods:index, create and store, UsersCreateRequest with validation, includes/display-form-errors snipet included in create view

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,1 +1,39 @@
-<h1>It is working admin/users/create</h1>
+@extends('layouts.admin')
+@section('content')
+    <h1>Create User</h1>
+
+    {!! Form::open(['method'=>"POST", 'action'=>"App\Http\Controllers\AdminUsersController@store"]) !!}
+    <div class="form-group">
+        {!! Form::label('name', 'Name') !!}
+        {!! Form::text('name', null, ['class'=>'form-control']) !!}
+    </div>
+    {!! '</br>' !!}
+    <div class="form-group">
+        {!! Form::label('email', 'Email') !!}
+        {!! Form::email('email', null, ['class'=>'form-control']) !!}
+    </div>
+    {!! '</br>' !!}
+    <div class="form-group">
+        {!! Form::label('password', 'Password') !!}
+        {!! Form::password('password', ['class'=>'form-control']) !!}
+    </div>
+    {!! '</br>' !!}
+    <div class="form-group">
+        {!! Form::label('role_id', 'Role') !!}
+        {!! Form::select('role_id', [''=>'Choose Option'] + $roles, null, ['class'=>'form-control']) !!}
+    </div>
+    {!! '</br>' !!}
+    <div class="form-group">
+        {!! Form::label('is_active', 'Status') !!}
+        {!! Form::select('is_active', [1=>'Active', 0=>'Not Active'], 0, ['class'=>'form-control']) !!}
+    </div>
+    {!! '</br>' !!}{!! '</br>' !!}
+    {{--//ZA SNIPET SUBMIT ////////////--}}
+    <div class="form-group">
+        {!! Form::submit('Create User',['class'=>'btn btn-primary']) !!}
+    </div>
+    {{--/////////////////////////////--}}
+    {!! Form::close() !!}
+      {!! '</br>' !!}
+    @include('includes.display-form-errors')
+@endsection
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,41 @@
-<h1>It is working admin/users/index</h1>
+@extends('layouts.admin')
+
+
+@section('content')
+    <h1>Users</h1>
+
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Role</th>
+            <th>Status</th>
+            <th>Created</th>
+        </tr>
+        </thead>
+        <tbody>
+        @if($users)
+            @foreach($users as $user)
+                <tr>
+                    <td>{{$user->name}}</td>
+                    <td>{{$user->email}}</td>
+                    <td>{{$user->role->name}}</td>
+                    <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
+                    <td>{{$user->created_at->diffForHumans()}}</td>
+                </tr>
+            @endforeach
+        @endif
+        </tbody>
+    </table>
+
+
+
+
+@endsection
+
+
+
+
+
+
